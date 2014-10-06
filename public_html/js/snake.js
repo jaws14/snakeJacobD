b685884db36e45e7f031600cbbb141dff039a897
pablo j delgado <fluffydemonkey@gmail>
2014-10-06 15:03:24 -0700

    i added some new code

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
 
+var context;
+var screenwidth;
+var screenheight;
 
+function gameinitialize() {
+    var canvas -
+}
+
+function gameloop() {
+    
+}
+
+function gamedraw() {
+    
+}
