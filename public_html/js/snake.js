303564f344d192d92371a689feae87267b4caa5d
pablo j delgado <fluffydemonkey@gmail>
2014-10-08 15:02:15 -0700

    i added some new code

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,17 +5,21 @@ var screenwidth;
 var screenheight;
 
 function gameinitialize() {
-    var canvas = document.getelementbyid("games-screen");
-    context = canvas.getcontext("2d");
+   var canvas = docuement.getelementid("game-screen");
+   context = canvas.getcontext("2d");
+   
+   screenWidth = window.innerWidth;
+   screenHeight = window.innerHeight;
+   
+   canvas.width = screenWidth;
+   canvas.height = screenHeight;
 }
-screenWidth = window.innerwidth;
-screenHeight = window.innerheight;
 
-function gameloop(){
-   canvas.width = screenwidth
-   canvas.height= screenheight
+function gameloop() {
+    
 }
 
 function gamedraw() {
-    
-}
+    context.fillStyle = "rgb(114, 0, 255)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}  
