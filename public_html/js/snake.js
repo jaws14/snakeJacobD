595ab5f2be2f3130cdaa6271d468e1c8833e6da1
pablo j delgado <fluffydemonkey@gmail>
2014-10-09 15:01:27 -0700

    i added some new code

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,8 +1,13 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenwidth;
 var screenheight;
+
+gameInitialize();
+gameDraw();
 
 function gameinitialize() {
    var canvas = docuement.getelementid("game-screen");
@@ -22,4 +27,22 @@ function gameloop() {
 function gamedraw() {
     context.fillStyle = "rgb(114, 0, 255)";
     context.fillRect(0, 0, screenWidth, screenHeight);
-}  
+}
+
+function snakeInitialize() {
+  snake = [];
+  snakeLength = 15;
+  snakeSize = 20;
+  
+  for() {
+      
+  }
+  }
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
+}
