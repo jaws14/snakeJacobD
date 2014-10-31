a3e3262e5cf3cbedd4b1f919f6f341589518ece3
pablo j delgado <fluffydemonkey@gmail>
2014-10-31 15:02:28 -0700

    added new code

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -12,6 +12,9 @@ var screenHeight;
 
 var gameState;
 var gameOverMenu;
+var restartButton;
+var playHUD;
+var scoreboard;
 /*-----------------------------------------------------------------------------
  * Function Callers- tell functions to activate.
  * ----------------------------------------------------------------------------
@@ -19,7 +22,7 @@ var gameOverMenu;
 gameInitialize();
 snakeInitialize();
 foodInitialize();
-setInterval(gameLoop, 30);
+setInterval(gameLoop, 80);
 /*-----------------------------------------------------------------------------
  * Functions- tell website what to do and can be used whenever.(these are game ones)
  * ----------------------------------------------------------------------------
@@ -38,6 +41,13 @@ function gameInitialize(){
     document.addEventListener("keydown", keyboardHandler);
     
     gameOverMenu = document.getElementById("gameOver");
+    centerMenuPosition(gameOverMenu);
+    
+    restartButton = document.getElementById("restartButton");
+    restartButton.addEventListener("click", gameRestart );
+    
+    playHUD = document.getElementById("playHUD");
+    scoreboard = document.getElementById("scoreboard");
     
     setState("PLAY");
    
@@ -49,6 +59,7 @@ function gameInitialize(){
 
 function gameLoop() {
     gameDraw();
+    drawScoreboard();
     if(gameState == "PLAY") {
     snakeUpdate();
     snakeDraw();
@@ -64,7 +75,15 @@ function gameDraw() {
     context.fillRect(0, 0, screenWidth, screenHeight);
     
     
-}           
+}
+
+function gameRestart() {
+    snakeInitialize();
+    foodInitialize();
+    hideMenu(gameOverMenu);
+    setState("PLAY");
+}
+
 /*-----------------------------------------------------------------------------
  * snake Functions
  * ----------------------------------------------------------------------------
@@ -99,6 +118,7 @@ function snakeUpdate(){
     
     checkFoodCollisions(snakeHeadX, snakeHeadY);
     checkWallCollisions(snakeHeadX, snakeHeadY);
+    checkSnakeCollisions(snakeHeadX, snakeHeadY);
     
     if(snakeDirection === "down") {
         snakeHeadY++;
@@ -175,12 +195,13 @@ function snakeUpdate(){
     
    
  function checkFoodCollisions(snakeHeadX, snakeHeadY) {
-     if(snakeHeadX == food.x && snakeHeadY == food.y) {
+     if(snakeHeadX === food.x && snakeHeadY === food.y) {
        snake.push({
             x: 0,
             y: 0
        });
        snakeLength++;
+       setFoodPosition();
      }
  }
  
@@ -190,17 +211,45 @@ function snakeUpdate(){
      }
  }
  
+ function checkSnakeCollisions(snakeHeadX, snakeHeadY) {
+     for(var index = 1; index < snake.length; index++) {
+         if(snakeHeadX == snake[index].x && snakeHeadY == snake[index].y) {
+             setState("GAME OVER");
+             return;
+         }
+     }
+ }
+ 
  function setState(state) {
      gameState = state;
      showMenu(state);
  }
  
+ 
+ 
+ 
  function displayMenu(menu) {
      menu.style.visibility = "visible";
+ }
+ 
+ function hideMenu(menu) {
+     menu.style.visibility = "hidden";
  }
  
  function showMenu(state) {
      if(state == "GAME OVER") {
          displayMenu(gameOverMenu);
      }
+     else if(state == "PLAY") {
+         displayMenu(playHUD);
+     }
+ }
+ 
+ function centerMenuPosition(menu) {
+     menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2) + "px";
+     menu.style.left = (screenWidth / 2) - (menu.offsetWidth / 2) + "px";
+ }
+ 
+ function drawScoreboard() {
+     scoreboard.innerHTML = "Length: " + snakeLength;
  }
